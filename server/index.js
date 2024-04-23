3b085682c97a431329e3b48fcee8e11a3ddd4eef
sup3r0001 <forsterandrew15@gmail.com>
2024-04-22 19:39:01 -0700

    Fixed Caching Issue

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ const pages = ['/', '/home', '/about', '/portfolio', '/skills'];
 
 pages.forEach(page => {
   app.get(page, (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
 });
